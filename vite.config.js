fc74a0fce9f1a4152732a6aaee505ffb6d4f6ef5
AMGewka <devid.gabibov@gmail.com>
2025-06-29 15:38:25 +0500

    Тестовый дроп

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/minecraft-rank-generator',
+  base: '/minecraft-rank-generator-RU/', // ✅ название репозитория на GitHub (с учётом регистра!)
   plugins: [vue()],
   build: {
     outDir: 'dist'
